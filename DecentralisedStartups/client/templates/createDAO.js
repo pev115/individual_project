b39dee23cd749ddc25a1862edd4f47662f87411e
pev115 <pev115@ic.ac.uk>
2016-06-27 17:32:00 +0100

    Create DAO button works - polished code a bit, focussing on just displaying address for now, need to see how not to show password and studd

--- DecentralisedStartups/client/templates/createDAO.js
+++ DecentralisedStartups/client/templates/createDAO.js
@@ -1,10 +1,17 @@
+/* Author: Eugene Valassakis
+ Description:
+ Logic that allows to create the contract and display its data.
+ Required: run the command :
+ geth --testnet --verbosity "2" --rpc --rpcapi "eth,net,web3,personal" --rpccorsdomain "http://localhost:3000" console
+ before connecting to the website.
 
-/*
+ //////////////////////////
+ //CONTRACT CODE:
+ ///////////////////////// 
  contract TestContract {
  uint public balance;
  uint public amount;
  address public owner;
-
 
  function TestContract(uint saidamount){
  amount = saidamount;
@@ -15,63 +22,34 @@
  function kill(){
  selfdestruct(owner);
  }
-
  }
-
-
-
- var saidamount = 15 ;
-var testcontractContract = web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"amount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"balance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[{"name":"saidamount","type":"uint256"}],"type":"constructor"}]);
-var testcontract = testcontractContract.new(
-        saidamount,
-        {
-            from: web3.eth.accounts[0],
-            data: '60606040526040516020806101cd833981016040528080519060200190919050505b806001600050819055503460006000508190555033600260006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908302179055505b5061015d806100706000396000f360606040526000357c01000000000000000000000000000000000000000000000000000000009004806341c0e1b51461005a5780638da5cb5b14610069578063aa8c217c146100a2578063b69ef8a8146100c557610058565b005b61006760048050506100e8565b005b6100766004805050610125565b604051808273ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390f35b6100af600480505061014b565b6040518082815260200191505060405180910390f35b6100d26004805050610154565b6040518082815260200191505060405180910390f35b600260009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16ff5b565b600260009054906101000a900473ffffffffffffffffffffffffffffffffffffffff1681565b60016000505481565b6000600050548156',
-            gas: 4700000
-        }, function (e, contract){
-            console.log(e, contract);
-            if (typeof contract.address !== 'undefined') {
-                console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-            }
-        })
-
-
-
-
-
-
-
-
+ /////////////////////
  */
 
 /* TODO:
  * rethink the personal api allowance and how could I structure it
  * make it so I can see the contract parameters on the screen*/
 
-
-Session.set('testSession',"Contract not created");
+/*Set the session with the data held before constructing the contract */
+Session.set('con_address',"Not Available - Please Create the contract");
 
 Template.createDAO.events({
-    'click #createDAOBtn': function(){
-       /* console.log("Button clicked!!!!!");
+    /* on click, create and deploy the contract */
+    'click #create_DAO_btn': function(){
+        /*Set the session to indicate that the contract is being mined */
+        Session.set('con_address',"Not Available - Waiting for miners");
 
-         Session.set('testSession2',"Now this string is changed!!");
-         var testSession= Session.get('testSession');
-         console.log(testSession);
-         console.log("Button clicked!!!!!");
+        /* Asking for password
+         * TODO: make a password prompt where not everyone can see what is happening
          */
-
-        Session.set('testSession',"Contract to be mined");
         var password = prompt("please input your password","password");
         web3.personal.unlockAccount(web3.eth.coinbase, password);
 
-
+        /* Deploy the contract */
         var saidamount = 15 ;
+        var DAO_contract = web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"amount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"balance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[{"name":"saidamount","type":"uint256"}],"type":"constructor"}]);
 
-
-        var testcontractContract = web3.eth.contract([{"constant":false,"inputs":[],"name":"kill","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[],"name":"amount","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[],"name":"balance","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[{"name":"saidamount","type":"uint256"}],"type":"constructor"}]);
-
-        var testContract = testcontractContract.new(
+        DAO_contract.new(
             saidamount,
             {
                 from: web3.eth.accounts[0],
@@ -80,16 +58,10 @@ Template.createDAO.events({
             }, function (e, contract){
                 console.log(e, contract);
                 if (typeof contract.address !== 'undefined') {
-                    console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
-                    Session.set('testSession', contract);
-                    console.log (contract);
+                    console.log('Contract mined! address: '+contract.address+' transactionHash: '+contract.transactionHash);
+                    Session.set('con_address',contract);
                 }
             })
-
-        console.log("here is the testcontract just created");
-        console.log(testContract);
-        /*Session.set('testSession',testContract);*/
-
     }
 });
 
@@ -97,13 +69,13 @@ Template.createDAO.events({
 
 
 Template.createDAO.helpers({
-    displayDAOInfo: function(){
-        var test_contract = Session.get('testSession');
+    DAO_address: function(){
+        var created_DAO = Session.get('con_address');
 
-        console.log(test_contract + "FAHHHHHHHHHHHHHHHHHHD");
-
-        if(typeof test_contract.address !== "string"){ return test_contract;
-        }else{        return test_contract.address;
+        if(typeof created_DAO.address !== 'undefined'){
+            return created_DAO.address;
+        }else{
+            return created_DAO;
         }
     }
 });
