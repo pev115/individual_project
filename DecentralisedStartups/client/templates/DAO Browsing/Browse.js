419ff3047099d755136d17f6487e1278f04096ff
pev115 <pev115@ic.ac.uk>
2016-07-14 22:38:55 +0100

    started working on filtering recruiting ect on browsing page , still incomplete

--- DecentralisedStartups/client/templates/DAO Browsing/Browse.js
+++ DecentralisedStartups/client/templates/DAO Browsing/Browse.js
@@ -2,6 +2,27 @@ Template.Browse.events({
     "submit #search": function (event) {
         event.preventDefault();
         Session.set("searchValue", $("#searchValue").val());
+
+
+        console.log("THE button");
+        console.log($("#searchInvestment"));
+        Session.set("searchInvestment", $("#searchInvestment").val());
+        Session.set("searchProducing", $("#searchProducing").val());
+        Session.set("searchRecruiting", $("#searchRecruiting").val());
+        Session.set("searchBuilding", $("#searchBuilding").val());
+
+      //  console.log(Session.get("searchInvestment"));
+        console.log(Session.get("searchProducing"));
+        //console.log(Session.get("searchRecruiting"));
+        //console.log(Session.get("searchBuilding"));
+    },
+    "click .btn-primary":function(event){
+        event.preventDefault();
+        console.log("HHHHHEEEERRREEE");
+        var _address = this.address;
+        var path = '/Monitor/'+_address;
+        console.log(path);
+        Router.go(path);
     }
 });
 
@@ -14,4 +35,10 @@ Template.Browse.helpers({
             return DAOs.find({});
         }
     }
+
+    /*,
+    daoLink:function(){
+        console.log("HHHHHEEEERRREEE");
+        console.log(this);
+    }*/
 });
